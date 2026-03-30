f391d5bd4a9a4ff402fb4da004b2349ca1dd52f4
iabbas96 <ibrahimabbas970@gmail.com>
2026-03-30 14:30:41 +0100

    feat: Added .env.local to the prject files ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Amplify } from 'aws-amplify'
-import outputs from '../amplify_outputs.json'
 import './index.css'
 
-Amplify.configure(outputs)
+//to get config from Environment Variable (Vercel) and a Fall back to the local file if the variable is missing (Local Dev)
+let config;
+if (import.meta.env.VITE_AMPLIFY_CONFIG) {
+  config = JSON.parse(import.meta.env.VITE_AMPLIFY_CONFIG);
+} else {
+  // for the local development to run
+  import('../amplify_outputs.json').then((module) => {
+    Amplify.configure(module.default);
+  }).catch(() => {
+    console.error("Amplify config not found");
+  });
+}
+
+if (config) Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
